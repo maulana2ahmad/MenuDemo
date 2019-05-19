5f4a0f096de6fb573e3c782240790e58fdac83e7
maulana2ahmad <maulana2ahmad@gmail.com>
2019-05-19 21:20:28 +0700

    Initial commit

---
+++ app/src/main/java/com/redudant/menudemo/MainActivity.java
@@ -0,0 +1,43 @@
+package com.redudant.menudemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+            case R.id.setting:
+                Log.i("Menu item selected", "setting");
+                return true;
+            case R.id.help:
+                Log.i("Menu item selected", "help");
+                return true;
+            default:
+                return false;
+        }
+    }
+}
